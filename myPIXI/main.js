3d0a68a85ec5b2e1d7d226b916427c29d0834678
Alena Pugovkina <alena@phoenix.local>
2018-05-09 10:40:39 +0300

    my PIXI anti-alias

--- myPIXI/main.js
+++ myPIXI/main.js
@@ -1,16 +1,47 @@
-
 // The application will create a renderer using WebGL, if possible,
 // with a fallback to a canvas render. It will also setup the ticker
 // and the root stage PIXI.Container
-const app = new PIXI.Application();
-console.log(app);
-// The application will create a canvas element for you that you
-// can then insert into the DOM
+renderer = PIXI.autoDetectRenderer(
+  300,
+  300,
+  {
+    // view: document.getElementById("animations-canvas"),
+    antialias: true
+  },
+  false, true
+);
 setTimeout(function(){
-  document.body.appendChild(app.view);
+  document.body.appendChild(renderer.view);
 }, 1000);
 
+var stage = new PIXI.Container();
+
+var circle = new PIXI.Graphics();
+circle.beginFill(0x1166FF);
+circle.drawCircle(60, 60, 150);
+circle.endFill();
+stage.addChild(circle);
+
+renderer.render(stage);
+
+// const app = new PIXI.Application();
+// console.log(app);
+// The application will create a canvas element for you that you
+// can then insert into the DOM
+// setTimeout(function(){
+//   document.body.appendChild(app.view);
+// }, 1000);
+
+/*let circle = new PIXI.Graphics();
+circle.beginFill(0x1166FF);
+circle.drawCircle(0, 10, 200);
+circle.endFill();
+circle.x = 64;
+circle.y = 130;
+app.stage.addChild(circle);*/
+
 // load the texture we need
+/*
 PIXI.loader.add('zombie', 'zombie.png').load((loader, resources) => {
   console.log("res: ", resources);
   // This creates a texture from a 'bunny.png' image
@@ -33,3 +64,4 @@ PIXI.loader.add('zombie', 'zombie.png').load((loader, resources) => {
     // bunny.rotation += 0.01;
   });
 });
+*/
